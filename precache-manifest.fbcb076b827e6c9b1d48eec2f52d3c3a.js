81e1e1076dd527171e914bb12f20bea472b30846
Bulinskimarek <you@example.combulipol@wp.pl>
2018-10-05 16:56:27 +0200

    Updates

---
+++ precache-manifest.fbcb076b827e6c9b1d48eec2f52d3c3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d3e39326d7fec48b6d1",
+    "url": "/Shibe/static/js/runtime~main.0d3e3932.js"
+  },
+  {
+    "revision": "5a44f8ca0f0077a878b8",
+    "url": "/Shibe/static/js/main.5a44f8ca.chunk.js"
+  },
+  {
+    "revision": "bc211f85ffa662b30ae2",
+    "url": "/Shibe/static/js/1.bc211f85.chunk.js"
+  },
+  {
+    "revision": "5a44f8ca0f0077a878b8",
+    "url": "/Shibe/static/css/main.5d4c1abe.chunk.css"
+  },
+  {
+    "revision": "1a6cfe06ed64aa52cd522baf6531cddf",
+    "url": "/Shibe/index.html"
+  }
+];
